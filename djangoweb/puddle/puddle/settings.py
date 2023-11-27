6f5cf0cc731ebd5ace8447ddd4fe0418552ac92b
evan0416 <cycloevan97@gmail.com>
2023-11-28 00:51:00 +0900

    code : Update  settings.py
    ALLOWED_HOSTS= []
    ->ALLOWED_HOSTS = [os.environ.get('HOST_WEB', 'localhost')]
    환경변수 추가

--- djangoweb/puddle/puddle/settings.py
+++ djangoweb/puddle/puddle/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
 from pathlib import Path
-
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-*r1k)tv-#zi_2au-@*dhz!1z*k4*99)k418ioe==(rkgy++5y+
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [os.environ.get('HOST_WEB', 'localhost')]
 
 LOGIN_URL = '/login/'
 LOGIN_REDIRECT_URL = '/'
