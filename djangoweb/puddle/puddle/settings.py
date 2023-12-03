53a83713febf97659dcd3036018fbb21a22ab0a1
dev.choi28 <timchoi931@gmail.com>
2023-12-03 21:13:13 +0900

    이미지 경로 수정

--- djangoweb/puddle/puddle/settings.py
+++ djangoweb/puddle/puddle/settings.py
@@ -146,7 +146,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = '/static/'
-MEDIA_URL = 'media/'
+MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR / 'media'
 
 
